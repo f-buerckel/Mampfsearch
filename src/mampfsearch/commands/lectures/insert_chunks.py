efd2f1c8d3357dd4c5efc8d2c29b052f31264b12
Florian <florianbuerckel@gmail.com>
2025-08-13 14:14:06 +0200

    Change insert command to use chunk objects instead of srt file

--- src/mampfsearch/commands/lectures/insert_chunks.py
+++ src/mampfsearch/commands/lectures/insert_chunks.py
@@ -1,35 +1,36 @@
 from mampfsearch.utils import config
+from mampfsearch.utils.models import Chunk
 from mampfsearch.utils import helpers
-import pysrt
-from tqdm import tqdm
-from pathlib import Path
+from typing import List
+from qdrant_client.models import PointStruct
 
-def insert_srt(lecture_name, lecture_position, srt_file, collection_name=config.LECTURE_COLLECTION_NAME):
+def insert_chunks(
+        chunks : List[Chunk],
+        collection_name=config.LECTURE_COLLECTION_NAME
+        ):
 
-    subs = pysrt.open(srt_file)
-
-    vectors, payloads = create_embeddings_and_payloads(subs, lecture_name, lecture_position)
-    upload(lecture_name, lecture_position, vectors, payloads, collection_name)
+    vectors, payloads = create_embeddings_and_payloads(chunks)
+    upload(vectors, payloads, collection_name)
 
     return 
 
-def create_embeddings_and_payloads(subs, lecture_name, lecture_position):
+def create_embeddings_and_payloads(chunks):
     model = config.get_bge_embedding_model()
 
     payloads = []
     vectors = []
 
-    for sub in tqdm(subs):
+    for chunk in chunks:
         payload = {
-            "text": str(sub.text),
-            "start_time": str(sub.start),
-            "end_time": str(sub.end),
-            "lecture_name": lecture_name,
-            "lecture_position" : lecture_position,
-            "position": int(sub.index)
+            "text": chunk.text,
+            "start_time": str(chunk.start_time),
+            "end_time": str(chunk.end_time),
+            "lecture_name": chunk.lecture_name,
+            "lecture_position" : chunk.lecture_position,
+            "position": chunk.position
         }
 
-        embedding = model.encode(str(sub.text),
+        embedding = model.encode(chunk.text,
                                     return_dense=True,
                                     return_sparse=True,
                                     return_colbert_vecs=True)
@@ -39,16 +40,15 @@ def create_embeddings_and_payloads(subs, lecture_name, lecture_position):
 
     return vectors, payloads
     
-def upload(lecture_name, lecture_position, vectors, payloads, collection_name):
-    from qdrant_client.models import PointStruct
+def upload(vectors, payloads, collection_name):
     client = config.get_qdrant_client()
 
-    for i, embedding in enumerate(tqdm(vectors)):
+    for i, embedding in enumerate(vectors):
         client.upsert(
             collection_name=collection_name,
             points = [
                 PointStruct(
-                    id=helpers.name_and_position_to_id(lecture_name, payloads[i]["position"], lecture_position),
+                    id=helpers.name_and_position_to_id(payloads[i]["lecture_name"], payloads[i]["position"], payloads[i]["lecture_position"]),
                     payload = payloads[i],
                     vector = {
                         "dense": embedding["dense_vecs"],
